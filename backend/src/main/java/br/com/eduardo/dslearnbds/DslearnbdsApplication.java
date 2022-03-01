e0a2d7693bdc18181d2925f834c3b40ec8f62178
Eduardo Alves dos Santos <santosedu@live.com>
2022-03-01 14:30:19 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/eduardo/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package br.com.eduardo.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
