e443ee297da5bcb838445aa0f2968c8fdb83214e
Eduardo Alves dos Santos <santosedu@live.com>
2022-03-06 21:57:47 -0300

    Enrollment e EnrollmentPK

--- backend/src/main/java/br/com/eduardo/dslearnbds/entities/Course.java
+++ backend/src/main/java/br/com/eduardo/dslearnbds/entities/Course.java
@@ -30,7 +30,6 @@ public class Course implements Serializable {
 	public Course() {}
 
 	public Course(Long id, String name, String imgUri, String imgGrayUri) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.imgUri = imgUri;
